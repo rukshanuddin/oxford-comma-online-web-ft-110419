5b0b6e5bee908d3b25b42cda2531fbcea5c38f39
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 19:19:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,6 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length == 3
-
-
-
-
-
   array[0..(array.length-2)].join(", ") + ", and " + array[array.length-1]
-
+end
 end
